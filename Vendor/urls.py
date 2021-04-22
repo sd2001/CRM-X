8d617d4374f36eea1552276125e7b5f1c5d1665f
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-22 20:44:00 +0530

    👌 IMPROVE: Adding the CRUD ops URLs for views

--- Vendor/urls.py
+++ Vendor/urls.py
@@ -5,5 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('products/', views.products,name="products"),
-    path('customers/<str:pk>', views.customer, name="customer")
+    path('customers/<str:pk>', views.customer, name="customer"),
+    path('createOrder', views.createOrder, name="createOrder"),
+    path('updateOrder/<str:pk>', views.updateOrder, name="updateOrder"),
+    path('deleteOrder/<str:pk>', views.deleteOrder, name="deleteOrder"),
+    path('updateProduct/<str:pk>', views.updateProduct, name="updateProduct"),
+    path('deleteProduct/<str:pk>', views.deleteProduct, name="deleteProduct"),
 ]
