6f89df6820ed714ba0532191eccf9e5c421e3daf
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-22 20:42:59 +0530

    📦 NEW: Added Django crispy forms

--- UltronCRM/settings.py
+++ UltronCRM/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Vendor'
+    'Vendor',
+    'crispy_forms'
 ]
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
