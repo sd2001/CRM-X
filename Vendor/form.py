89dd546d19e2970221b7fee2f9edd298071f1cdf
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-24 11:56:23 +0530

    ‼️ BREAKING: Updated with User Login Auth

--- Vendor/form.py
+++ Vendor/form.py
@@ -1,5 +1,8 @@
 from django.forms import ModelForm
 from .models import *
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class OrderForm(ModelForm):
     class Meta:
@@ -10,3 +13,9 @@ class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = '__all__'
+        
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+    
--- Vendor/urls.py
+++ Vendor/urls.py
@@ -3,7 +3,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path('', views.home, name="home"),    
+    path('login/', views.login_user, name="login"),    
+    path('signup/', views.signup, name="signup"),    
+    path('logout/', views.logout_user, name="logout"),    
+    path('home/', views.dashboard, name="dashboard"),
     path('products/', views.products,name="products"),
     path('customers/<str:pk>', views.customer, name="customer"),
     path('createOrder/<str:pk>', views.createOrder, name="createOrder"),
