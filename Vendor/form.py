3e1c29f51fd1cc70328ac521e8aad06860b7fb2e
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-27 18:19:59 +0530

    ⚡️ Update product instance fix

--- Vendor/form.py
+++ Vendor/form.py
@@ -9,6 +9,22 @@ class OrderForm(ModelForm):
         model = Order
         fields = '__all__'
         
+        # exclude = ['customer']
+
+class OrderFormCustomer(ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(OrderFormCustomer, self).__init__(*args, **kwargs)
+        instance = getattr(self, 'instance', None)      
+        self.fields['status'].disabled = True        
+        # self.fields['customer'].disabled = True        
+    class Meta:
+        model = Order
+        # fields = ['product']
+        fields = '__all__'
+        exclude = ['customer']
+        
+        
+        
 class ProductForm(ModelForm):
     class Meta:
         model = Product
@@ -18,4 +34,11 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        
+        
+class CustomerUpdate(ModelForm):
+    class Meta:
+        model = Customer
+        fields = '__all__'
+        exclude = ['user']
     
