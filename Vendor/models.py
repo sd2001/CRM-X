a97eff398adae47097fac5fcb28532757180c8cc
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-27 18:20:20 +0530

    ✨ Added support for Images

--- Vendor/models.py
+++ Vendor/models.py
@@ -1,14 +1,17 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 # Create your models here.
 class Customer(models.Model):
-	name = models.CharField(max_length=50, null=True)
-	phone = models.CharField(max_length=50, null=True)
-	email = models.CharField(max_length=50, null=True)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, null=True)
+    phone = models.CharField(max_length=50, null=True)
+    email = models.CharField(max_length=50, null=True)
+    image = CloudinaryField('image', default="https://res.cloudinary.com/sd2001/image/upload/v1619428574/samples/default_wzltoh.jpg", null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
  
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, null=True)
@@ -16,30 +19,30 @@ class Tag(models.Model):
         return self.name
 
 class Product(models.Model):
-	CATEGORY = (
-			('Entertainment', 'Entertainment'),
-			('Clothing', 'Clothing'),
-			) 
+    CATEGORY = (
+            ('Entertainment', 'Entertainment'),
+            ('Clothing', 'Clothing'),
+            ) 
 
-	name = models.CharField(max_length=200, null=True)
-	price = models.FloatField(null=True)
-	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description = models.CharField(max_length=200, null=True)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	tag = models.ManyToManyField(Tag)
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tag = models.ManyToManyField(Tag)
     
-	def __str__(self):
-		return self.name
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
-	STATUS = (
-			('Pending', 'Pending'),
-			('Out for delivery', 'Out for delivery'),
-			('Delivered', 'Delivered'),
-			)
+    STATUS = (
+            ('Pending', 'Pending'),
+            ('Out for delivery', 'Out for delivery'),
+            ('Delivered', 'Delivered'),
+            )
 
-	customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
-	product = models.ForeignKey(Product,null=True, on_delete=models.SET_NULL)
-	date_created = models.DateTimeField(auto_now_add=True, null=True)
-	status = models.CharField(max_length=200, null=True, choices=STATUS)
-	
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product,null=True, on_delete=models.SET_NULL)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200,default='Pending', null=True, choices=STATUS)
+    
