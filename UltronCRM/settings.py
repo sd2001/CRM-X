2a2c5b3b0315aaee6f6d28d887555852733f6749
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-27 18:48:32 +0530

    🐛 Collectstatic deploy Bug

--- UltronCRM/settings.py
+++ UltronCRM/settings.py
@@ -128,10 +128,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles') ]
+
+STATICFILES_DIRS = [
+    f"{BASE_DIR}/static"
+]
 
 # Default primary key field type
 # https:/
