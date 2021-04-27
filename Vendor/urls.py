ccf5bdf5f767676c673072c672771fe726829228
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-27 18:22:31 +0530

    ✨ Added proper names for urls

--- Vendor/urls.py
+++ Vendor/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from . import views
 
@@ -18,4 +19,8 @@ urlpatterns = [
     path('deleteOrder/<str:pk>', views.deleteOrder, name="deleteOrder"),
     path('updateProduct/<str:pk>', views.updateProduct, name="updateProduct"),
     path('deleteProduct/<str:pk>', views.deleteProduct, name="deleteProduct"),
+    # path('reset_password/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    # path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
