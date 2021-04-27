06c4ebd182d02620c26e14c18f11f7a158dd5ce9
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-27 18:19:36 +0530

    🚨 Added support for Django Signals

--- Vendor/apps.py
+++ Vendor/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class VendorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Vendor'
+    
+    def ready(self):
+        import Vendor.signals
