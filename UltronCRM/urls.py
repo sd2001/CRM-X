44c1da1a9f93817eb6f4023956a017e6c8e55fb8
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-28 17:48:09 +0530

    ✨Adding support for Django Defender

--- UltronCRM/urls.py
+++ UltronCRM/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('Vendor.urls'))
+    path('', include('Vendor.urls')),
+    path('admin/defender/', include('defender.urls')),
 ]
