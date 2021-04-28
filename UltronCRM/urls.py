ed551ef01061d6e5a74eeb91ab54221d93921d81
Swarnabha Das <im.swarnabha2001@gmail.com>
2021-04-28 18:19:34 +0530

    🔥  Fixed Django Defender redis bugs

--- UltronCRM/urls.py
+++ UltronCRM/urls.py
@@ -19,5 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('Vendor.urls')),
-    path('admin/defender/', include('defender.urls')),
-]
+],
